56170f4726d028dfa2260a1c16a6225a89c31534
austin.davis <austin.davis@hii-tsd.com>
2024-12-30 20:28:49 -0600

    Adding material and nav bar component

---
+++ PlexVideoConverter/ClientApp/pvc-app/src/app/modules/material.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ]
+}) export class MaterialModule {}
